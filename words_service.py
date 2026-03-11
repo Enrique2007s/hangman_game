3820534d61dfbb802007a458e3d117bc5116d497
Enrique2007s <enriquesmith458@gmail.com>
2026-03-11 00:31:27 -0400

    installed requests(forgot to earlier). Started working on word requests from API

--- words_service.py
+++ words_service.py
@@ -1,10 +1,21 @@
 import random
+import requests
+
+API_URL = "https://random-word-api.herokuapp.com/word?number=1"
+
 
 def get_word():
     """
     Reads a list of words from a words.txt file
     and returns a random word.
     """
-    with open("words.txt" , "r") as file:
+    try:
+        response = requests.get(API_URL, timeout=5)
+        if response.status_code == 200:
+            return response.json()[0]
+    except requests.RequestException as e:
+        print(f"Error fetching word from API: {e}")
+
+    with open("words.txt", "r") as file:
         words = file.read().splitlines()
     return random.choice(words)
