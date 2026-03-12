ef1f07b6f07e9727dba3b7d6d4f8dae41b481988
Enrique2007s <enriquesmith458@gmail.com>
2026-03-12 14:02:23 -0400

    deleted main.py and replaced its content into run.py

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Welcome to Hangman Game!")
+from game import play_game
+
+
+def main():
+    """
+    This is the main function that starts the Hangman game.
+    """
+    print("Welcome to Hangman!")
+    print("Try to guess the word, one letter at a time.")
+    print("You have 6 incorrect guesses before you lose.")
+    print("Good luck!")
+    print("-----------------------------------\n")
+    play_game()
+
+
+if __name__ == "__main__":
+    main()
